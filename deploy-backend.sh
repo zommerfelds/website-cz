a84bfb325b604eb0cc3b7e8702e02ff0876ee604
zommerfelds <zommerfelds@users.noreply.github.com>
2017-12-14 20:32:13 -0500

    deployment improvements

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
 
+sls=$(npm bin)/sls
 (cd backend; npm install)
-sls deploy
-URL=$(sls info | grep POST | awk '{ print $NF }')
+$sls deploy
+URL=$($sls info | grep POST | awk '{ print $NF }')
 echo "var deploymentData = {contactUrl: \"$URL\"};" > client/src/js/2-deploymentData.js
-node client/build.js
-sls client deploy
+
