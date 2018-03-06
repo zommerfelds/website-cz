5c9103671106142b6191fdfa540588b6197c7d29
zommerfelds <zommerfelds@users.noreply.github.com>
2018-03-06 02:00:34 -0500

    consistent linting

--- client/build.js
+++ client/build.js
@@ -1,6 +1,7 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
-const permalinks  = require('metalsmith-permalinks');
+const permalinks = require('metalsmith-permalinks');
 const sass = require('metalsmith-sass');
 const pug = require('metalsmith-pug');
 const ignore = require('metalsmith-ignore');
@@ -10,7 +11,7 @@ const layouts = require('metalsmith-layouts');
 
 require('./src/js/2-deploymentData'); // just make sure it exists
 
-const devMode = (process.env.DEV_MODE === "true");
+const devMode = (process.env.DEV_MODE === 'true');
 console.log('Dev mode:', devMode);
 
 metalsmith(__dirname)
@@ -18,45 +19,48 @@ metalsmith(__dirname)
     title: 'Christian Zommerfelds',
     description: 'It\'s about saying »Hello« to the World!',
     url: 'http://uiaeuiaeuiae',
-    disableRecaptcha: devMode
+    disableRecaptcha: devMode,
   })
   .source('./src')
   .destination('./dist')
   // .clean(false)
   .use(sass({
-    outputDir: 'css/'
+    outputDir: 'css/',
   }))
   .use(markdown())
   .use(collections({
     posts: 'posts/**',
     sortBy: 'date',
-    reverse: true
+    reverse: true,
   }))
   .use(ignore('layouts/**'))
-  .use((files, m, done) => { // there is a cyclical dependency between the plugins, so let's fix some permalinks before we run pug
-    for (const post of m.metadata().posts) {
-      if (post.path.endsWith('.html')) {
-        post.path = post.path.slice(0, -'.html'.length);
+  .use((files, m, done) => {
+    // there is a cyclical dependency between the plugins,
+    // so let's fix some permalinks before we run pug
+    const { posts } = m.metadata();
+    for (let i = 0; i < posts.length; i += 1) {
+      if (posts[i].path.endsWith('.html')) {
+        posts[i].path = posts[i].path.slice(0, -'.html'.length);
       }
     }
     done();
   })
   .use(pug({
     pretty: devMode,
-    useMetadata: true
+    useMetadata: true,
   }))
   .use(permalinks({
-    relative: false
+    relative: false,
   }))
   .use(layouts({
     directory: 'src/layouts',
     default: 'post.pug',
-    pattern: 'posts/**'
+    pattern: 'posts/**',
   }))
   .use(uglify({
     root: 'js',
-    concat: {file: 'scripts.min.js'},
-    removeOriginal: true
+    concat: { file: 'scripts.min.js' },
+    removeOriginal: true,
   }))
   .build((err) => {
     if (err) { throw err; }
