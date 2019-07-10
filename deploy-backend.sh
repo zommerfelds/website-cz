74abc21b90b8e63a4d51a3d49737147a76b355fa
Christian Zommerfelds <zommerfelds@users.noreply.github.com>
2019-07-11 01:23:06 +0200

    Temporarily print js files

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -7,3 +7,5 @@ $sls deploy
 URL=$($sls info | grep POST | awk '{ print $NF }')
 echo -e "/* eslint-disable */\n/* THIS FILE IS AUTO-GENERATED, DO NOT EDIT */\nvar deploymentData = {contactUrl: '$URL'};" > client/src/js/2-deploymentData.js
 
+# TODO: remove next line
+ls client/src/js
