4970f31f67cdd34ed724654cd213fe9e0b68dc22
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-07-18 07:39:59 +0000

    Check serverless output

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -3,5 +3,8 @@ set -ex
 
 npm install
 npx sls deploy
+
 URL=$(npx sls info | grep POST | awk '{ print $NF }')
+# NOTE: sls seems buggy and doesn't return an error exit code
+[[ -z "$URL" ]] && { echo "Deployment URL is empty" ; exit 1; }
 echo -e "/* eslint-disable */\n/* THIS FILE IS AUTO-GENERATED, DO NOT EDIT */\nvar deploymentData = {contactUrl: '$URL'};" > client/src/js/2-deploymentData.js
