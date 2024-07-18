b1d749106140d659870c24d380ca0d5d1cc98639
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-07-18 07:20:48 +0000

    Enable debugging

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -2,6 +2,8 @@
 set -ex
 
 npm install
-npx sls deploy
+env | grep SERVERLESS_ACCESS_KEY
+
+npx sls --debug deploy
 URL=$(npx sls info | grep POST | awk '{ print $NF }')
 echo -e "/* eslint-disable */\n/* THIS FILE IS AUTO-GENERATED, DO NOT EDIT */\nvar deploymentData = {contactUrl: '$URL'};" > client/src/js/2-deploymentData.js
