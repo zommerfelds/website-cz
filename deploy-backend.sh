681acdec36b92eef6938a540a7e937ab18671e8d
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-07-18 07:30:47 +0000

    More fixing

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -2,8 +2,6 @@
 set -ex
 
 npm install
-env | grep SERVERLESS_ACCESS_KEY
-
-npx sls --debug deploy
+npx sls deploy
 URL=$(npx sls info | grep POST | awk '{ print $NF }')
 echo -e "/* eslint-disable */\n/* THIS FILE IS AUTO-GENERATED, DO NOT EDIT */\nvar deploymentData = {contactUrl: '$URL'};" > client/src/js/2-deploymentData.js
