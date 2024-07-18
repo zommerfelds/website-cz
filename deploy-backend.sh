a3c87800ab8d7179423c3b409b765bf5e5b587f3
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-07-18 07:12:11 +0000

    More trying to fix CI

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-sls=$(npm bin)/sls
 npm install
-$sls deploy
-URL=$($sls info | grep POST | awk '{ print $NF }')
+npx sls deploy
+URL=$(npx sls info | grep POST | awk '{ print $NF }')
 echo -e "/* eslint-disable */\n/* THIS FILE IS AUTO-GENERATED, DO NOT EDIT */\nvar deploymentData = {contactUrl: '$URL'};" > client/src/js/2-deploymentData.js
